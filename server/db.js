d7d8d2bfb997bfff07853bbccdfe261a6ff4d783
syarifabdllh <syarifudinabdllh@gmail.com>
2025-12-29 22:54:12 +0800

    db js ulang

--- server/db.js
+++ server/db.js
@@ -1,23 +1,13 @@
-const mysql = require('mysql2');
+// File: server/db.js
+const mysql = require('mysql2/promise');
 require('dotenv').config();
-
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 28882, // Port default Aiven
-  ssl: {
-    rejectUnauthorized: false // Wajib ditambahkan untuk Aiven
-  }
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
-
-db.connect((err) => {
-  if (err) {
-    console.error('Gagal koneksi ke Aiven:', err.message);
-    return;
-  }
-  console.log('Terhubung ke database Aiven MySQL!');
-});
-
-module.exports = db;
+module.exports = pool;
